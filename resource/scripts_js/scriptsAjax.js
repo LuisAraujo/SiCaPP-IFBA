63414dcfbc05cb7b39a829893e9b4158e2e3eb84
Luis4raujo <luisaraujo.ifba@gmail>
2015-08-28 15:48:26 -0300

    Implementando  FontEnd
    Form Login, Cadastro Pesquisador, Cadastro Bolsista

--- resource/scripts_js/scriptsAjax.js
+++ resource/scripts_js/scriptsAjax.js
@@ -1,3 +1,42 @@
 /**
  * Created by Luis 4raujo on 25/08/15.
  */
+
+
+/**** LOGIN USUÁRIO ****/
+$("#lu_form").validate({
+
+    submitHandler: function(form) {
+
+        var dados = $(form).serialize();
+
+        $.ajax({
+            type: "POST",
+
+            url: "class/Controller/Dispatcher.php?classe=Login&acao=logar",
+            data: dados,
+            success: function(data)
+            {
+                if (data=="0")
+                    redireciona(AMB_BOL);
+                else if (data=="1")
+                    redireciona(AMB_PES);
+                else
+                   //usar um span na view para informar ao usuário
+                   console.log("usuário ou senha incorreto!");
+            },
+            error: function(data)
+            {
+                console.log("Erro ao obter dados: "+data);
+            }
+        });
+
+        return false;
+    }
+
+
+});
+
+
+
+
--- resource/scripts_js/scriptsInteracao.js
+++ resource/scripts_js/scriptsInteracao.js
@@ -1,3 +1,15 @@
 /**
  * Created by Luis 4raujo on 25/08/15.
  */
+
+AMB_BOL = "/amb_bolsita";
+AMB_PES = "/amb_pesquisa";
+AMB_ADM = "/admin";
+
+/**
+ * Função de redirecionamento de página usada no login
+ * @param param CONST
+ */
+redireciona = function(param){
+    console.log(param);
+}
