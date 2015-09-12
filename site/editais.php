a2a0d793990fa46f71134f96f2bdda2d67a7ddfd
Luis4raujo <luisaraujo.ifba@gmail>
2015-09-12 02:55:21 -0300

    Ambiente Pesquisador
    Lista de Editais, Sistema que força download de arquivos

---
+++ site/editais.php
@@ -0,0 +1,32 @@
+<?php
+
+$edital = $_GET["a"];
+$nomeArquivo = $edital.".pdf";
+$caminhoArquivo = "../uploads/editais/".$nomeArquivo;
+
+if(isset($caminhoArquivo ) && file_exists($caminhoArquivo )){
+    // faz o teste se a variavel não esta vazia e se o arquivo realmente existe
+    switch(strtolower(substr(strrchr(basename($caminhoArquivo ),"."),1))){
+    // verifica a extensão do arquivo para pegar o tipo
+    case "pdf": $tipo="application/pdf"; break;
+    case "exe": $tipo="application/octet-stream"; break;
+    case "zip": $tipo="application/zip"; break;
+    case "doc": $tipo="application/msword"; break;
+    case "xls": $tipo="application/vnd.ms-excel"; break;
+    case "ppt": $tipo="application/vnd.ms-powerpoint"; break;
+    case "gif": $tipo="image/gif"; break;
+    case "png": $tipo="image/png"; break;
+    case "jpg": $tipo="image/jpg"; break;
+    case "mp3": $tipo="audio/mpeg"; break;
+    case "php": ;// deixar vazio por seurança
+    case "htm": ;// deixar vazio por seurança
+    case "html": ; // deixar vazio por seurança
+    }
+    header("Content-Type: ".$tipo); // informa o tipo do arquivo ao navegador
+    header("Content-Length: ".filesize($caminhoArquivo )); // informa o tamanho do arquivo ao navegador
+    header("Content-Disposition: attachment; filename=".basename($caminhoArquivo )); // informa ao navegador que é tipo anexo e faz abrir a janela de download, tambem informa o nome do arquivo
+    readfile($caminhoArquivo ); // lê o arquivo
+    exit; // aborta pós-ações }
+}
+?>
+
